a4f0e890a33f1b5404d41fcc06f306fae47aaf6e
Brian <bhw2113@columbia.edu>
2016-11-09 15:28:55 -0800

    updates for swift 3, embrace integration

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+
+target 'swift-2048' do
+  use_frameworks!
+
+  pod 'EmbraceIO'
+
+  target 'swift-2048Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- swift-2048/ViewController.swift
+++ swift-2048/ViewController.swift
@@ -7,16 +7,22 @@
 //
 
 import UIKit
+import Embrace
 
 class ViewController: UIViewController {
                             
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Embrace.sharedInstance().endAppStartup()
+    }
 
-  @IBAction func startGameButtonTapped(sender : UIButton) {
-    let game = NumberTileGameViewController(dimension: 4, threshold: 2048)
-    self.presentViewController(game, animated: true, completion: nil)
-  }
+    @IBAction func startGameButtonTapped(_ sender : UIButton) {
+        Embrace.sharedInstance().beginEvent(withName: "game_load")
+        let game = NumberTileGameViewController(dimension: 4, threshold: 2048)
+        self.present(game, animated: true, completion: {
+            Embrace.sharedInstance().endEvent(withName: "game_load")
+        })
+    }
+    
 }
 
